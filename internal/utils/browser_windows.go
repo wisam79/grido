7ee6dc301e240146a66046d592d84d981d7a290d
wisam79 <73247302+wisam79@users.noreply.github.com>
2026-07-13 21:05:44 +0300

    fix: restore cmd.exe for opening browser to fix auth window not opening on Windows

--- internal/utils/browser_windows.go
+++ internal/utils/browser_windows.go
@@ -17,5 +17,5 @@ func OpenBrowser(targetURL string) error {
 	if u.Scheme != "http" && u.Scheme != "https" {
 		return errors.New("unsupported protocol scheme")
 	}
-	return exec.Command("rundll32", "url.dll,FileProtocolHandler", targetURL).Start()
+	return exec.Command("cmd", "/c", "start", "", targetURL).Start()
 }
