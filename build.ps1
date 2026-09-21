6e678a806795d9c5c2b2c5a1b974b2f45efae745
wisam79 <73247302+wisam79@users.noreply.github.com>
2026-09-21 15:36:50 +0300

    rel: release v1.4.1

--- build.ps1
+++ build.ps1
@@ -42,7 +42,7 @@ if (-not $appVersion) {
     $appVersion = (git describe --tags --always 2>$null)
 }
 if (-not $appVersion) {
-    $appVersion = "v1.4.0"
+    $appVersion = "v1.4.1"
 }
 
 Write-Host " [2/3] Building Wails v3 Desktop App ($appVersion)..." -ForegroundColor Green
